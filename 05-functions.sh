43309749b26dd18e43f603f06d2275691ec44e4f
Anto <japanto20@gmail.com>
2023-04-05 15:19:42 -0500

    create servers script updated

--- 05-functions.sh
+++ 05-functions.sh
@@ -2,8 +2,12 @@
 
 xyz(){
   echo Hello from function
+  echo First Argument - $1
+  echo First Argument -- $2
+  echo All Arguments - $*
+  echo No of Arguments - $#
 }
 
 # call a function
 
-xyz
+xyz 123 456
