6f036d617dafbc1e75653c3bee7f8f55b8a7add0
Anto <japanto20@gmail.com>
2023-04-05 15:23:29 -0500

    create servers script updated

--- 05-functions.sh
+++ 05-functions.sh
@@ -2,12 +2,15 @@
 
 xyz(){
   echo Hello from function
-  echo First Argument - $1
-  echo First Argument -- $2
-  echo All Arguments - $*
+  echo First Argument - "$1"
+  echo First Argument -- "$2"
+  echo All Arguments - "$*"
   echo No of Arguments - $#
+  echo Value of a = "$a"
+  b=200
 }
 
 # call a function
-
+a=120
 xyz 123 456
+echo Value of b = $b
