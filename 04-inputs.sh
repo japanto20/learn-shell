97b4b2564bc9fef3f9123e7bf6484d65399cf3fe
Anto <japanto20@gmail.com>
2023-04-05 12:32:26 -0500

    create servers script updated

--- 04-inputs.sh
+++ 04-inputs.sh
@@ -1,2 +1,10 @@
-echo $1
-echo $2
+# 0-n , $*/
+echo  first Argument $1 # first Argument
+echo second argument $2 # second argument
+
+echo script name $0 # script name
+
+echo All Arguments = $*
+echo All Arguments = $@ # All Arguments
+
+echo Number of Arguments = $# # Number of Arguments
