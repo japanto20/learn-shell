1156db38643965810d6784acefda1d8a28f9a4bf
Anto <japanto20@gmail.com>
2023-04-06 15:48:46 -0500

    create servers script updated

--- 06-conditions.sh
+++ 06-conditions.sh
@@ -3,11 +3,15 @@ string=$2
 
 if [ "${number}" -eq 5 ]; then
   echo number is 5
+else
+  echo number is not 5
 fi
 
 # shellcheck disable=SC2154
 if [ "${string}" == abc ]; then
   echo string is abc
+else
+  echo string is not abc
 fi
 
 #it is always a good practice to quote variables in expressions
